18eaacc155973ec9c7ae1bf6f14ea5d14260a401
TomaszOrda <tomaszorda@outlook.com>
2024-11-14 11:22:21 +0100

    Update strategyScanning.py
    Cleaned up the comments (as mentioned in README.md comments seem necessary in scanning version, as it is much more obfuscated than dnc one).

--- src/strategyScanning.py
+++ src/strategyScanning.py
@@ -6,7 +6,7 @@ def strategyScanning(rates, margin=0.05, currency_1 = "CURR1", currency_2 = "CUR
   
   potentialBuyingDay = 0
   
-  if opening_currency == currency_2:# and closing_currency == currency_1:
+  if opening_currency == currency_2:
     rates = list(map(lambda x: 1/x, rates))
 
   def buySellIncome(buyRate, sellRate):
@@ -14,7 +14,7 @@ def strategyScanning(rates, margin=0.05, currency_1 = "CURR1", currency_2 = "CUR
     
   
   for day in range(len(rates)):
-    if rates[day]<rates[potentialBuyingDay]: # new possible buying day
+    if rates[day]<rates[potentialBuyingDay]: # A better buying day found for some future buy-sell pair, as such there is no need for remmebering the previously considered buying day
       potentialBuyingDay = day
     
     potentialBuySellIncome = buySellIncome(rates[potentialBuyingDay], rates[day])
@@ -25,23 +25,21 @@ def strategyScanning(rates, margin=0.05, currency_1 = "CURR1", currency_2 = "CUR
         potentialBuyingDay = day
       continue
     
-    if potentialBuySellIncome>max(rates[day]/rates[optimalStrategy[-1]], 1): # we can have new buysell pair that is better than postponing buying and positive
+    if potentialBuySellIncome>max(rates[day]/rates[optimalStrategy[-1]], 1): # We can have new buysell pair that is better than postponing buying and positive
       optimalStrategy.extend([potentialBuyingDay, day])
       potentialBuyingDay = day
-    elif rates[day] - rates[optimalStrategy[-1]] > 0: # we can postpone selling
+    elif rates[day] - rates[optimalStrategy[-1]] > 0: # We can postpone selling
        optimalStrategy[-1] = day
        potentialBuyingDay = day
 
-  if opening_currency != closing_currency: #we will need additional buy or one less sell
+  if opening_currency != closing_currency: # We will need additional buy or one less sell
     if len(optimalStrategy)==0:
         return [potentialBuyingDay]
     betterBuyingDay = potentialBuyingDay if rates[potentialBuyingDay] < rates[optimalStrategy[-2]] else optimalStrategy[-2]
-    if potentialBuyingDay != optimalStrategy[-1] and buySellIncome(rates[optimalStrategy[-2]], rates[optimalStrategy[-1]]) * 1/rates[potentialBuyingDay] * (1-margin) > 1/rates[betterBuyingDay] * (1-margin): #check if we get more from additional potential buying or from dropping last selling day (for the favor of potential or last buying day?)
+    if potentialBuyingDay != optimalStrategy[-1] and buySellIncome(rates[optimalStrategy[-2]], rates[optimalStrategy[-1]]) * 1/rates[potentialBuyingDay] * (1-margin) > 1/rates[betterBuyingDay] * (1-margin): # Check if we get more from additional potential buying or from dropping last selling day (for the favor of potential or last buying day?)
       optimalStrategy.append(potentialBuyingDay)
-      #add potential buying day to strategy
     else:
       del optimalStrategy[-2:]
       optimalStrategy.append(betterBuyingDay)
-      #remove last buy and sell, add better buying day 
 
   return optimalStrategy
